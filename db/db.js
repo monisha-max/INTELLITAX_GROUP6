d215499350628299cb76b953d9d4983265d65358
monisha-max <115718081+monisha-max@users.noreply.github.com>
2024-03-22 22:38:59 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,10 +1,9 @@
 // db.js
 const mongoose = require('mongoose');
-const dbName = 'TaxCollector';
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://monishakollipara13:lQCoLu5hP6mIuaoj@cluster0.viy9cup.mongodb.net/TaxCollector?retryWrites=true&w=majority&appName=Cluster0' + dbName, {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
